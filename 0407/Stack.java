c0119a6fd906a9d7bf4202f6fa63b13d4ba34f19
gkwovlf123 <74278391+gkwovlf123@users.noreply.github.com>
2021-04-07 20:01:57 +0900

    Add files via upload

---
+++ 0407/Stack.java
@@ -0,0 +1,14 @@
+package dataStructure;
+
+interface Stack {
+	boolean isEmpty();
+	void push(char item);
+	char pop();
+	void delete();
+	char peek();
+}
+class StackNode{
+	char data;
+	StackNode link;
+}
+
