a654255f53492c09b10f27f9932d1392f5adf8c7
gkwovlf123 <74278391+gkwovlf123@users.noreply.github.com>
2021-04-14 22:37:16 +0900

    Add files via upload

---
+++ 0414/Queue_test.java
@@ -0,0 +1,26 @@
+package dataStructure;
+
+public class Queue_test {
+
+	public static void main(String[] args) {
+		int QueueSize=3;
+		char deletedItem;
+		ArrayQueue Q = new ArrayQueue(QueueSize);
+		
+		Q.enQueue('A');
+		Q.printQueue();
+		
+		Q.enQueue('B');
+		Q.printQueue();
+		
+		deletedItem = Q.deQueue();
+		if(deletedItem != 0)
+			System.out.println("deleted Item : " + deletedItem);
+		Q.printQueue();
+		
+		Q.enQueue('C');
+		Q.printQueue();
+		
+	}
+
+}
